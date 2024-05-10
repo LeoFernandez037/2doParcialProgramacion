a4e87e064731b5a07c18aae7dbc56ba76e2f3675
LeoFernandez037 <leonelfernandezuwu@gmail.com>
2024-05-10 09:32:14 -0400

    composicion de componentes

--- src/componentes/ImgAndButton.jsx
+++ src/componentes/ImgAndButton.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const ImgAndButton = () => {
   return (
     <div>
-      <img src="https://ibb.co/rQWxynW" alt="imagen no encontrada" />
+      <img
+        src="https://i.ibb.co/S6LtvqL/8bed0ee79117e6d26de911a4ac519346.jpg"
+        alt="imagen no encontrada"
+      />
       <button>
         <img
           src="https://th.bing.com/th/id/OIP.pWqpWwU1a7923Xm1-2iisAHaGh?rs=1&pid=ImgDetMain"
