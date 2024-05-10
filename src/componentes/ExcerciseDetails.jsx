45d43f109d1ee21a3d093decdeb3424b7239521b
LeoFernandez037 <leonelfernandezuwu@gmail.com>
2024-05-10 09:23:58 -0400

    segundo componente

--- src/componentes/ExcerciseDetails.jsx
+++ src/componentes/ExcerciseDetails.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ExcerciseDetails = () => {
-  return <div>ExcerciseDetails</div>;
+  return (
+    <div>
+      <h2>Terapia 2</h2>
+      <h3>Ejercicios Basicos</h3>
+    </div>
+  );
 };
 
 export default ExcerciseDetails;
---
+++ src/componentes/ImgAndButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ImgAndButton = () => {
+  return (
+    <div>
+      <img src="https://ibb.co/rQWxynW" alt="imagen no encontrada" />
+      <button>
+        <img
+          src="https://th.bing.com/th/id/OIP.pWqpWwU1a7923Xm1-2iisAHaGh?rs=1&pid=ImgDetMain"
+          alt="imagen noencontrada"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default ImgAndButton;
